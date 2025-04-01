0626773b39bd035c1f6c71f7826203e6b2d5a68a
Azure-Heights <shining.azure.heights@gmail.com>
2025-04-01 10:12:06 -0600

    Brainstorming.

---
+++ eval.py
@@ -0,0 +1,6 @@
+"""
+Metrics:
+    * Character development - will humans relate to their journey?
+    * Novelty - how much is similar/different to other stories?
+    * Cohesive theme
+"""
---
+++ gen.py
@@ -0,0 +1,17 @@
+"""
+Definition for plot sketch:
+    * Story hook
+    * Scene for every step of the hero's journey
+        * Scene:
+            * Time
+            * Location/Environment
+            * Characters present
+            * Importance to plot
+            * What actually happens (broad strokes)
+    * At least:
+        * 1 main character
+        * 1 antagonist
+    * No more than:
+        * 2 antagonists
+        * 5 prominent supporting characters
+"""
